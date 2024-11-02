23653b5cf97769dbe4859867297f77fd08f0d1e9
Eray Aşina <162134586+asinaeray@users.noreply.github.com>
2024-11-02 16:46:59 +0300

    desig
    dedsfd

--- public/themes/hackathon/home/home.php
+++ public/themes/hackathon/home/home.php
@@ -1,166 +1,168 @@
-<?php
-require_once $_SERVER["DOCUMENT_ROOT"] . '/src/Config/config.php';
-require_once AUTOLOAD_PATH;
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+  <head><script src="../assets/js/color-modes.js"></script>
 
-use App\Operation\AIOperation;
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.122.0">
+    <title>Anasayfa</title>
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $requestData = $_POST;
-    $userModel = new AIOperation();
-    $response = $userModel->chatBot($requestData);
-    header('Content-Type: application/json');
-    echo json_encode($response);
-    exit; // PHP işlemi burada sona erdir
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sohbet Alanı</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/jumbotron/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/styles/atom-one-dark.min.css">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.5.1/highlight.min.js"></script>
+
+    
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
-                @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
 
-        * {
-            font-family: "Poppins", sans-serif;
-            font-weight: 500;
-            font-style: normal;
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
         }
-       
-        body {
-            background: #f6f6f6;
-            color: white;
-            font-family: "Montserrat Alternates", sans-serif;
-        }
+      }
 
-        .navbar {
-            border-radius: 10px;
-            padding: 15px;
-            margin-top: 10px;
+      .b-example-divider {
+        width: 100%;
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
 
-        }
-        .navbar,
-        .sidebar {
-            background-color: #e5efff;
-        }
-        .cato:hover,
-        .nav-link:hover {
-            color: #00dbde;
-        }
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
 
-        .chat-container {
-            background-color: #e5efff;
-            padding: 20px;
-            border-radius: 5px;
-            overflow-y: auto;
-        }
-        .chat-message {
-            margin: 5px 0;
-        }
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
 
-        .user {
-            color: #2ba0ff;
-            /* Kullanıcı mesaj rengi */
-        }
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
 
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
 
-        .sidebar {
-            padding: 20px;
-            border-radius: 10px;
-        }
+      .btn-bd-primary {
+        --bd-violet-bg: #712cf9;
+        --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
 
-        @media (max-width: 768px) {
-            .sidebar {
-                margin-bottom: 20px;
-            }
-        }
-        .cato:hover,
-        .nav-link:hover {
-            color: #00dbde;
-        }
-        
-        .bg-header {
-            background: linear-gradient(45deg, #0000007d, #0c044d66), url("https://images.pexels.com/photos/1229042/pexels-photo-1229042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
-            background-repeat: no-repeat;
-            background-position: center;
-            background-size: cover;
-            color: #ccc;
-        }
-        .cato:hover,
-        .nav-link:hover {
-            color: #00dbde;
-        }
-        .btn{
-            width: 100%;
-            background-color: black;
-            color: white;
-        }
+        --bs-btn-font-weight: 600;
+        --bs-btn-color: var(--bs-white);
+        --bs-btn-bg: var(--bd-violet-bg);
+        --bs-btn-border-color: var(--bd-violet-bg);
+        --bs-btn-hover-color: var(--bs-white);
+        --bs-btn-hover-bg: #6528e0;
+        --bs-btn-hover-border-color: #6528e0;
+        --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
+        --bs-btn-active-color: var(--bs-btn-hover-color);
+        --bs-btn-active-bg: #5a23c8;
+        --bs-btn-active-border-color: #5a23c8;
+      }
+
+      .bd-mode-toggle {
+        z-index: 1500;
+      }
+
+      .bd-mode-toggle .dropdown-menu .active .bi {
+        display: block !important;
+      }
     </style>
-</head>
 
-<body>
+    
+  </head>
+  <body>
 
-<header class="header mb-5">
-        <nav class="navbar navbar-expand-lg container">
-            <a class="navbar-brand" href="/">Shark Edu</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ml-auto">
-                    <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="chat">ChatBot</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
-                    <li class="nav-item"><a class="nav-link" href="login"><span class="material-icons-outlined">account_circle</span></a></li>
-                </ul>
-            </div>
-        </nav>
+
+
+
+    
+<main>
+  <div class="container py-4">
+    <header class="pb-3 mb-4 border-bottom">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">About</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            AI
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">ChatBot</a></li>
+            <li><a class="dropdown-item" href="#">Software Test</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
     </header>
-    <div class="container mt-5">
-        <div class="row">
-        <div class="col-md-3 sidebar">
-                <h5 class="filter-title text-center" style=" background: #0078ff; border-radius: 20px; background: linear-gradient(45deg, #00dbde, #7AB2D3);">Kategoriler</h5>
-                <div class="Kategoriler">
-                    <a href="#" class="cato"> - Yazılım Dünyası</a>
-                </div>
-                <!-- Diğer kategoriler -->
-            </div>
 
-            <div class="col-md-9">
-                <div class="chat-container" id="chat-container">
-                    <div id="chat-messages">
-                        <div class="row">
-                            <div class="col-lg-5 m-3">
-                            <div class="card" style="width: 18rem;">
-                              <img class="card-img-top" src="..." alt="Card image cap">
-                              <div class="card-body">
-                                <h5 class="card-title">Başlık</h5>
-                                <p class="card-text">Açıklama</p>
-                                <a href="#" class="btn">Katıl</a>
-                              </div>
-                            </div>
-                            </div>
-                            </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="p-5 mb-4 bg-body-tertiary rounded-3">
+      <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">Custom jumbotron</h1>
+        <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
+        <button class="btn btn-primary btn-lg" type="button">Talk To AI</button>
+      </div>
     </div>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-   
 
-</body>
+      <div class="col-md-6">
+        <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+          <h2>Title</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta facilis enim eum vel, odio possimus aspernatur harum rerum consectetur non nihil voluptatem porro maxime explicabo? Aliquam aspernatur ea tempore corrupti.</p>
+          <button class="btn btn-outline-success" type="button">Join</button>
+        </div>
+      </div>
+    </div>
 
+  </div>
+</main>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+    </body>
 </html>
